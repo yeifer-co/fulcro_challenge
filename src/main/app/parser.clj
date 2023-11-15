7076b6422b9a2af432b8da71ba9e0f0697fc3180
yeifer-co <developer@yeifer.co>
2023-11-14 22:53:48 -0500

    🔁(📡): Refactor app.parser to use app.resolvers

--- src/main/app/parser.clj
+++ src/main/app/parser.clj
@@ -1,18 +1,11 @@
 (ns app.parser
   (:require
+    [app.resolvers]
     [com.wsscode.pathom.core :as p]
     [com.wsscode.pathom.connect :as pc]
     [taoensso.timbre :as log]))
 
-; Test data
-
-;(pc/defresolver latest-task [_ _]
-;                {::pc/output [{::latest-task [:task/id :task/description :task/completed]}]}
-;                {::latest-task {:task/id          3
-;                                :task/description "Task Example"
-;                                :task/completed   true}})
-
-(def resolvers [])
+(def resolvers [app.resolvers/resolvers])
 
 (def pathom-parser
   (p/parser
@@ -28,4 +21,38 @@
 
 (defn api-parser [query]
   (log/info "Process" query)
-  (pathom-parser {} query))
+  (pathom-parser {} query)
+  )
+
+(comment
+  (require 'app.parser)
+
+  ;;; Given a :task/id, return data for that task
+  ;(pc/defresolver get-task [env {:task/keys [id]}]
+  ;                {::pc/input  #{:task/id}
+  ;                 ::pc/output [:task/id :task/description :task/completed :task/list-id]}
+  ;                (get tasks-table id))
+  ;
+
+  ; Call get-task resolver
+  (app.parser/api-parser [{[:task/id 2] [:task/id
+                                         :task/description
+                                         :task/completed
+                                         :task/list-id
+                                         ]}])
+
+  ;;; Given a :task-list/id, this can generate a list item-count and the tasks
+  ;;; in that list (but just with their IDs)
+  ;(pc/defresolver get-task-list [env {:task-list/keys [id]}]
+  ;                {::pc/input  #{:task-list/id}
+  ;                 ::pc/output [:task-list/id :task-list/item-count {:task-list/tasks [:task/id]}]}
+  ;                (when-let [task-list (get tasks-list-table id)]
+  ;                  (assoc task-list
+  ;                         :task-list/tasks (mapv (fn [id] {:task/id id}) (:task-list/tasks task-list)))))
+
+  ; Call get-task-list resolver
+  (app.parser/api-parser [{[:task-list/id 1] [:task-list/id
+                                              :task-list/item-count
+                                              {:task-list/tasks [:task/id]}]}])
+
+  )

52772bc0826e37da400d1a4b5051792c3cd76d8f
yeifer-co <developer@yeifer.co>
2023-11-15 22:00:32 -0500

    🔁(📡): Refactor mutations and resolvers to use Datomic API

--- src/main/app/mutations.clj
+++ src/main/app/mutations.clj
@@ -1,13 +1,86 @@
 (ns app.mutations
   (:require
-    [app.resolvers :refer [tasks-list-table]]
     [com.wsscode.pathom.connect :as pc]
-    [taoensso.timbre :as log]))
+    [datomic.api :as d]
+    [taoensso.timbre :as log]
+    [app.schema :as schema]))
 
-(pc/defmutation delete-task [env {task-list-id :task-list/id
-                                  task-id :task/id}]
+(defn retract-task-by-id [env id]
+  (let [conn (schema/get-conn env)
+        db (schema/get-db env)
+        get-task-ident-q (:get-task-ident-q schema/queries)
+        task-ident (ffirst (d/q get-task-ident-q db id))]
+    (log/info "Retracting task" id)
+    @(d/transact conn [[:db/retract task-ident :task/id id]]))
+  nil) ; Return nil to avoid returning the task map (it is not handled by the mutation)
+
+(defn retract-task-from-list [env list-id task-id]
+  (let [conn (schema/get-conn env)
+        db (schema/get-db env)
+        get-task-list-ident-q (:get-task-list-ident-q schema/queries)
+        task-list-ident (ffirst (d/q get-task-list-ident-q db list-id))]
+    (log/info "Retracting task" task-id "from list" list-id)
+    @(d/transact conn [[:db/retract task-list-ident :task-list/tasks task-id]]))
+  nil) ; Return nil to avoid returning the task map (it is not handled by the mutation)
+
+(pc/defmutation delete-task [env {:keys [id list-id]}]
                 {::pc/sym `delete-task}
-                (log/info "Delete task" task-id "from task list" task-list-id)
-                (swap! tasks-list-table update task-list-id update :task-list/tasks (fn [old-list] (filterv #(not= task-id %) old-list))))
+                (log/info "Calling delete-task mutation")
+                (retract-task-by-id env id)
+                (retract-task-from-list env list-id id))
 
-(def mutations [delete-task])
+(defn transact-task-completed [env id new-value]
+  (let [conn (schema/get-conn env)
+        db (schema/get-db env)
+        get-task-ident-q (:get-task-ident-q schema/queries)
+        task-ident (ffirst (d/q get-task-ident-q db id))]
+    (log/info "Updating task" id "field `completed` to (" new-value ")")
+    @(d/transact conn [{:db/id task-ident :task/completed new-value}]))
+  nil) ; Return nil to avoid returning the task map (it is not handled by the mutation)
+
+(pc/defmutation edit-completed [env {:keys [id new-value]}]
+                {::pc/sym `edit-completed}
+                (log/info "Calling edit-completed mutation")
+                (transact-task-completed env id new-value))
+
+(defn transact-task-description [env id new-value]
+  (let [conn (schema/get-conn env)
+        db (schema/get-db env)
+        get-task-ident-q (:get-task-ident-q schema/queries)
+        task-ident (ffirst (d/q get-task-ident-q db id))]
+    (log/info "Updating task" id "field `description` to (" new-value ")")
+    @(d/transact conn [{:db/id task-ident :task/description new-value}]))
+  nil) ; Return nil to avoid returning the task map (it is not handled by the mutation
+
+(pc/defmutation edit-description [env {:keys [id new-value]}]
+                {::pc/sym `edit-description}
+                (log/info "Calling edit-description mutation")
+                (transact-task-description env id new-value))
+
+(defn transact-new-task [env list-id description]
+  (let [conn (schema/get-conn env)
+        db (schema/get-db env)
+        get-task-list-ident-q (:get-task-list-ident-q schema/queries)
+        task-list-item-count-q (:task-list-item-count-q schema/queries)
+        task-list-ident (ffirst (d/q get-task-list-ident-q db list-id))
+        new-id (inc (ffirst (d/q task-list-item-count-q db list-id)))
+        temp-id (d/tempid :db.part/user)
+        ]
+    (log/info "Adding new task" new-id "to list" list-id)
+    @(d/transact conn [{:db/id temp-id :task/id           new-id
+                                      :task/description  description
+                                      :task/completed    false
+                                      :task/list-id      list-id}
+                       [:db/add task-list-ident :task-list/tasks new-id]
+                       [:db/add task-list-ident :task-list/item-count new-id]]))
+  nil) ; Return nil to avoid returning the task map (it is not handled by the mutation
+
+(pc/defmutation add-task [env {:keys [list-id description]}]
+                {::pc/sym `add-task}
+                (log/info "Calling add-task mutation")
+                (transact-new-task env list-id description))
+
+(def mutations [delete-task
+                edit-completed
+                edit-description
+                add-task])
--- src/main/app/resolvers.clj
+++ src/main/app/resolvers.clj
@@ -1,41 +1,57 @@
 (ns app.resolvers
   (:require
-    [com.wsscode.pathom.core :as p]
     [com.wsscode.pathom.connect :as pc]
-    ))
+    [datomic.api :as d]
+    [taoensso.timbre :as log]
+    [app.schema :as schema]))
 
-(def tasks-table
-  (atom {1 {:task/id          1
-      :task/description "Do the dishes"
-      :task/completed   true
-      :task/list-id     1}
-   2 {:task/id          2
-      :task/description "Take out the trash"
-      :task/completed   false
-      :task/list-id     1}
-   3 {:task/id          3
-      :task/description "Make the bed"
-      :task/completed   true
-      :task/list-id     1}}))
-
-(def tasks-list-table
-  (atom {1 {:task-list/id    1
-      :task-list/item-count  3
-      :task-list/tasks       [1 2 3]}}))
+;; Build a task map using a query to get a task by id from the database
+(defn get-task-by-id [env id]
+  (let [db      (schema/get-db env)
+        map-keys [:task/id :task/description :task/completed :task/list-id]
+        task-by-id-q (:task-by-id-q schema/queries)]
+    (log/info "Query and processing [ :task" id "] from database")
+    (->> (d/q task-by-id-q db id)                           ; Query the database
+         (first)                                            ; Get the first result (there should only be one)
+         (rest)                                             ; Remove the first element (the entity id), it is handled by query
+         (zipmap map-keys)                                  ; Zipmap the results to a map
+         )))
 
 ;; Given a :task/id, return data for that task
 (pc/defresolver get-task [env {:task/keys [id]}]
                 {::pc/input  #{:task/id}
                  ::pc/output [:task/id :task/description :task/completed :task/list-id]}
-                (get @tasks-table id))
+                (log/info "Calling get-task resolver")
+                (get-task-by-id env id))
 
-;; Given a :task-list/id, this can generate a list item-count and the tasks
-;; in that list (but just with their IDs)
+(defn mvec-to-mmap [keys multi-vec]
+  "Convert a vector of vectors to a map of maps using keys for the inner maps values
+  It is assumed that the first element of each vector is the entity id, so it is discarded"
+  (map (fn [m-vec]
+         (zipmap keys (rest m-vec))) multi-vec))
+
+(defn get-task-list-by-id [env id]
+  (let [db      (schema/get-db env)
+        map-keys [:task-list/id :task-list/item-count :task-list/tasks]
+        task-list-by-id-q (:task-list-by-id-q schema/queries)
+        results (d/q task-list-by-id-q db id)]
+    ; Results:> [[17592186045422 1 3 3], [17592186045422 1 3 2], [17592186045422 1 3 1]]
+    ; Target format: #:task-list{:id 1, :item-count 3, :tasks [#:task{:id 1} #:task{:id 2} #:task{:id 3}]}
+    ; Generate the map in target format
+    (log/info "Query and processing [ :task-list" id "] from database")
+    (let [mmap-task-list (mvec-to-mmap map-keys results)
+          task-list (select-keys (first mmap-task-list) [:task-list/id :task-list/item-count])]
+      (->> mmap-task-list
+           (map :task-list/tasks)
+           (mapv (fn [id] {:task/id id}))
+           (assoc task-list :task-list/tasks)))))
+
+;; Given a :task-list/id, this can generate a list id item-count and the tasks
 (pc/defresolver get-task-list [env {:task-list/keys [id]}]
                 {::pc/input  #{:task-list/id}
                  ::pc/output [:task-list/id :task-list/item-count {:task-list/tasks [:task/id]}]}
-                (when-let [task-list (get @tasks-list-table id)]
-                  (assoc task-list
-                         :task-list/tasks (mapv (fn [id] {:task/id id}) (:task-list/tasks task-list)))))
+                (log/info "Calling get-task-list resolver")
+                (get-task-list-by-id env id))
 
-(def resolvers [get-task get-task-list])
+(def resolvers [get-task
+                get-task-list])

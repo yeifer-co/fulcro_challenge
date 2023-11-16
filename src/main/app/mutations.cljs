25a4f7cff30979cfa86b424de7fcebe74e5ec72f
yeifer-co <developer@yeifer.co>
2023-11-15 23:43:38 -0500

    🔁(🔮): Refactor add-task mutation to use new-task map.

--- src/main/app/mutations.cljs
+++ src/main/app/mutations.cljs
@@ -23,15 +23,12 @@
 (defmutation add-task [{:keys [list-id description]}]
   (action
     [{:keys [state]}]
-    (let [new-id (inc (get-in @state [:task-list/id list-id :task-list/item-count]))]
-      #_(swap! state update-in [:task-list/id list-id :task-list/tasks] conj {:task/id           new-id
-                                                                            :task/description  description
-                                                                            :task/completed    false
-                                                                            :task/list-id      list-id})
-      ; Use merge/merge-ident to add the new task to the task-list/tasks vector
-      (swap! state merge/merge-ident [:task/id new-id] {:task/id           new-id
-                                                        :task/description  description
-                                                        :task/completed    false
-                                                        :task/list-id      list-id})
+    (let [new-id (inc (get-in @state [:task-list/id list-id :task-list/item-count]))
+          new-task {:task/id           new-id
+                    :task/description  description
+                    :task/completed    false
+                    :task/list-id      list-id}]
+      (swap! state merge/merge-ident [:task/id new-id] new-task)
+      (swap! state update-in [:task-list/id list-id :task-list/tasks] conj new-task)
       (swap! state update-in [:task-list/id list-id :task-list/item-count] inc)))
   (remote [env] true))
